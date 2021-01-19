eb28d6c803b203230e6028e8ef55f8ad08dcbaf6
Simon Aertssen <Simonaertssen@gmail.com>
2021-01-20 00:05:25 +0100

    ready to start popping?

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import time
 import schedule
 from speedtest import Speedtest
 
+from Storage import AddResultsToDataBaseTable
+from WiFi import GetNetworkData, GetActiveNetworkName, ConnectTo
+
 
 def PerformSpeedtest():
     s = Speedtest()
@@ -9,21 +12,34 @@ def PerformSpeedtest():
     s.get_best_server()
     s.download()
     s.upload()
-    return s.results.timestamp,
-           s.results.server.host,
-           s.results.server.id,
-           s.results.download,
-           s.results.upload,
-           s.results.ping
+    return s.results.dict()
 
 def RunTest():
-    for ssid, passwrd in GetNetworkData():
-        
+    for desired_ssid, passwrd in GetNetworkData():
+        ConnectTo(desired_ssid, passwrd)
+        try:
+            data = PerformSpeedtest()
+        except Error as e:
+            continue
+        ssid = GetActiveNetworkName() # Should be same as desired but connection might have failed
 
+        results = {}
+        results["TIMESTAMP"]     = data["timestamp"]
+        results["WIFINAME"]      = ssid
+        results["HOSTNAME"]      = data["server"]["host"]
+        results["HOSTID"]        = data["server"]["id"]
+        results["DOWNLOADSPEED"] = data["download"]
+        results["UPLOADSPEED"]   = data["upload"]
+        results["LATENCY"]       = data["ping"]
+
+        AddResultsToDataBaseTable(desired_ssid, results)
+        print(f"Test on {ssid} successful..")
+
+def ScheduleAllTests():
+    schedule.every(30).minutes.do(RunTest)
+    while True:
+        schedule.run_pending()
+        time.sleep(10)
 
 if __name__ == '__main__':
-    PerformSpeedtest()
-    # schedule.every(1).seconds.do(TestSchedule)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(0.1)
+    ScheduleAllTests()
